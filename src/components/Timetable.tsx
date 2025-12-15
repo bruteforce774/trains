144ff07e567c54c31b1d767085eb8668fe52d4b8
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-15 14:00:44 +0100

    start on timetable component

---
+++ src/components/Timetable.tsx
@@ -0,0 +1,10 @@
+import type { Train } from '../types'
+type TimetableProps = { trains: Train[] };
+
+function Timetable({ trains }: TimetableProps) { 
+    return (
+        <></>
+    )
+}
+
+export default Timetable
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface Train {
+    uuid: string;
+    trainIdent: string;
+    startStation: string;
+    endStation: string;
+    currentStation: string;
+    departure: string;
+    arrival: string;
+} 
